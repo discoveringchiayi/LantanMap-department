f0276f2061a83619b7499ae5a1dda32b149aa80d
discoveringchiayi <discoveringchiayi@gmail.com>
2026-05-30 23:35:12 +0800

    新儲存庫

---
+++ 食品加工廠/data.js
@@ -0,0 +1,465 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1a-14",
+      "name": "1(接A-14)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.670705179866534,
+        "pitch": 0.0031537573709030653,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0772343139079457,
+          "pitch": 0.17552292129223268,
+          "rotation": 0,
+          "target": "1-2d-12"
+        },
+        {
+         "yaw": -0.0520536, "pitch": 0.0182766, "rotation": 0,
+          "target": "12--1",
+          "urlTarget": "https://discoveringchiayi.github.io/map-of-Lantan-campus/A大學路/app-files/index.html#12--1",
+          "title": "前往 食品加工廠 側門"
+        }       
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2d-12",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8646176310120346,
+          "pitch": 0.08265509080729139,
+          "rotation": 0,
+          "target": "0-1a-14"
+        },
+        {
+          "yaw": 1.4080734054968165,
+          "pitch": 0.17781741607902823,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": -3.108542777846706,
+          "pitch": 0.2666910614387774,
+          "rotation": 0,
+          "target": "5-6"
+        },
+         {
+          "yaw": -0.2293529, "pitch": 0.0392821, "rotation": 0,
+          "target": "12--1",
+          "urlTarget": "https://discoveringchiayi.github.io/map-of-Lantan-campus/D1-14(完成)/app-files/index.html#12--1",
+          "title": "前往 食品加工廠 門口"
+        } 
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.479682091102637,
+          "pitch": 0.26819957976001874,
+          "rotation": 0,
+          "target": "1-2d-12"
+        },
+        {
+          "yaw": -0.5746780490097549,
+          "pitch": 0.253025763830113,
+          "rotation": 0,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.324832306762083,
+        "pitch": 0.0011994878861276703,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0152344604299373,
+          "pitch": 0.33922575353769346,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": -1.3356327163309896,
+          "pitch": 0.14861365044301245,
+          "rotation": 12.566370614359176,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3302317638731758,
+          "pitch": -0.05812997160618494,
+          "title": "培育實驗室",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.1648112068195431,
+        "pitch": 0.09044674713994283,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.340997179550506,
+          "pitch": 0.050635418482936956,
+          "rotation": 4.71238898038469,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.0743590267992023,
+        "pitch": -0.193160202153976,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4752016021817482,
+          "pitch": 0.07704286656873549,
+          "rotation": 0,
+          "target": "6-7"
+        },
+        {
+          "yaw": 0.28507970700963625,
+          "pitch": 0.16389096426687821,
+          "rotation": 0,
+          "target": "1-2d-12"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4683464088618372,
+          "pitch": -0.09367891576172838,
+          "title": "農產品加值打樣中心",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-7",
+      "name": "7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20908246393965513,
+          "pitch": 0.06107194197759824,
+          "rotation": 0,
+          "target": "5-6"
+        },
+        {
+          "yaw": 0.23174681731599733,
+          "pitch": 0.09925292042908751,
+          "rotation": 1.5707963267948966,
+          "target": "7-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-8",
+      "name": "8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.008278006157144802,
+        "pitch": 0.2847047044977451,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5383990071278255,
+          "pitch": 0.20192221482939132,
+          "rotation": 0,
+          "target": "6-7"
+        },
+        {
+          "yaw": 2.4494532415794694,
+          "pitch": 0.20290859022746233,
+          "rotation": 0.7853981633974483,
+          "target": "8-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-9",
+      "name": "9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.3000397093212026,
+        "pitch": 0.09281293819579162,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0774466307076507,
+          "pitch": 0.04306042732941506,
+          "rotation": 4.71238898038469,
+          "target": "7-8"
+        },
+        {
+          "yaw": -2.0399301291124203,
+          "pitch": 0.05197244053275618,
+          "rotation": 0.7853981633974483,
+          "target": "9-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10",
+      "name": "10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.029148190245358307,
+        "pitch": 0.1649835205247996,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8531408298868897,
+          "pitch": 0.19263682016626404,
+          "rotation": 6.283185307179586,
+          "target": "8-9"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
